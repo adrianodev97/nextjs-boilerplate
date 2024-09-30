dbfc1f736690e9b808fa1c6556235063616f1866
Adriano Junior <adrianojunior.dev97@gmail.com>
2024-09-30 12:31:56 -0300

    adjust: fix build erros and add not found page

--- src/store/themeModeReducers/slice.ts
+++ src/store/themeModeReducers/slice.ts
@@ -1,11 +1,24 @@
 "use client";
 
+import type { IThemeMode } from "@/styles/theme/types";
 import { createSlice } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
 import type { IThemeModeState } from "./types";
 
+const getThemeMode = () => {
+	let themeMode: IThemeMode = "light";
+
+	if (typeof window !== "undefined") {
+		const storedThemeMode = window.localStorage.getItem("themeMode");
+		if (storedThemeMode === "dark" || storedThemeMode === "light") {
+			themeMode = storedThemeMode;
+		}
+	}
+	return themeMode;
+};
+
 const initialState: IThemeModeState = {
-	mode: localStorage.getItem("themeMode") === "dark" ? "dark" : "light",
+	mode: getThemeMode(),
 };
 
 const themeModeSlice = createSlice({
@@ -14,7 +27,9 @@ const themeModeSlice = createSlice({
 	reducers: {
 		toggleThemeMode(state) {
 			state.mode = state.mode === "dark" ? "light" : "dark";
-			localStorage.setItem("themeMode", state.mode);
+			if (typeof window !== "undefined") {
+				window.localStorage.setItem("themeMode", state.mode);
+			}
 		},
 	},
 });
