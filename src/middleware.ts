ccf5b2a9c44c807220de64f373410e6e14035615
Adriano Junior <88254828+adrianodev97@users.noreply.github.com>
2024-10-15 00:28:06 -0300

    fix: normalize the pathname to avoid set locale under locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,9 +22,18 @@ export function middleware(request: NextRequest) {
 	const { pathname } = request.nextUrl;
 	const pathnameParts = pathname.split("/").filter(Boolean);
 	const pathnameHasLocale =
-		pathnameParts.length > 0 && locales.includes(pathnameParts[0] as ILocale);
+		pathnameParts.length > 0 &&
+		locales.includes(pathnameParts[0].toLowerCase() as ILocale);
 
-	if (pathnameHasLocale) return;
+	if (pathnameHasLocale) {
+		const locale = pathnameParts[0].toLowerCase();
+		const normalizedPathname = `/${locale}${pathname.substring(locale.length + 1)}`;
+		if (pathname !== normalizedPathname) {
+			request.nextUrl.pathname = normalizedPathname;
+			return NextResponse.redirect(request.nextUrl);
+		}
+		return;
+	}
 
 	const locale = getLocale(request);
 	request.nextUrl.pathname = `/${locale}${pathname}`;
