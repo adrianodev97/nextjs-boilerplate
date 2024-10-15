d277d51faf0741908a81cc970f4e0ddf6ecbb407
Adriano Junior <88254828+adrianodev97@users.noreply.github.com>
2024-10-14 22:11:05 -0300

    refactor: change palette colors

--- src/styles/theme/palette.ts
+++ src/styles/theme/palette.ts
@@ -1,14 +1,15 @@
-import type { PaletteOptions } from "@mui/material/styles";
+import { type PaletteOptions, alpha } from "@mui/material/styles";
 import type { IThemeMode } from "./types";
 
 export const lightPalette: PaletteOptions = {
 	primary: {
-		main: "#11F8D6",
-		light: "#46FDE2",
-		dark: "#04D3B4",
+		main: "#027AF2",
+		light: "#99CCFF",
+		dark: "#0059B3",
+		contrastText: "#E5F2FF",
 	},
 	secondary: {
-		main: "#BFD4ED",
+		main: "#DBECFF",
 		light: "#DBECFF",
 		dark: "#AEC6E1",
 	},
@@ -23,9 +24,10 @@ export const lightPalette: PaletteOptions = {
 		dark: "#F49F1F",
 	},
 	info: {
-		main: "#0288D1",
-		light: "#03A9F4",
-		dark: "#01579B",
+		main: "#4DA6FF",
+		light: "#D6EBFF",
+		dark: "#1C8CFD",
+		contrastText: "#F5F6FA",
 	},
 	success: {
 		main: "#4BB543",
@@ -48,17 +50,23 @@ export const lightPalette: PaletteOptions = {
 		default: "#ffffff",
 		paper: "#f5f5f5",
 	},
+	divider: alpha("#C2C9D6", 0.4),
 	text: {
 		primary: "#212121",
 		secondary: "#5F5C6B",
+	},
+	action: {
+		hover: alpha("#DADEE7", 0.2),
+		selected: `${alpha("#DADEE7", 0.3)}`,
 	},
 };
 
 export const darkPalette: PaletteOptions = {
 	primary: {
-		main: "#BB86FC",
-		light: "#D0A1FF",
-		dark: "#9A67EA",
+		main: "#027AF2",
+		light: "#4DA6FF",
+		dark: "#0059B3",
+		contrastText: "#E5F2FF",
 	},
 	secondary: {
 		main: "#03DAC6",
@@ -76,9 +84,10 @@ export const darkPalette: PaletteOptions = {
 		dark: "#C49000",
 	},
 	info: {
-		main: "#2196F3",
-		light: "#6EC6FF",
-		dark: "#0069C0",
+		main: "#0059B3",
+		light: "#026BD4",
+		dark: "#00366B",
+		contrastText: "#4DA6FF",
 	},
 	success: {
 		main: "#4CAF50",
@@ -86,24 +95,29 @@ export const darkPalette: PaletteOptions = {
 		dark: "#087F23",
 	},
 	grey: {
-		"50": "#fafafa",
-		"100": "#eeeeee",
-		"200": "#e0e0e0",
-		"300": "#C8C8CA",
-		"400": "#8C8A97",
-		"500": "#7A7786",
-		"600": "#5F5C6B",
-		"700": "#4E4B59",
-		"800": "#34323E",
-		"900": "#212121",
+		"50": "#F5F6FA",
+		"100": "#EBEEF4",
+		"200": "#DADEE7",
+		"300": "#C2C9D6",
+		"400": "#94A0B8",
+		"500": "#566481",
+		"600": "#47536B",
+		"700": "#333B4D",
+		"800": "#0B0E14",
+		"900": "#05070A",
 	},
 	background: {
-		default: "#121212",
-		paper: "#1E1E1E",
+		default: "#05070A",
+		paper: "#0C1017",
 	},
+	divider: alpha("#333B4D", 0.6),
 	text: {
 		primary: "#E0E0E0",
 		secondary: "#B0B0B0",
+	},
+	action: {
+		hover: alpha("#47536B", 0.2),
+		selected: `${alpha("#47536B", 0.3)}`,
 	},
 };
 
